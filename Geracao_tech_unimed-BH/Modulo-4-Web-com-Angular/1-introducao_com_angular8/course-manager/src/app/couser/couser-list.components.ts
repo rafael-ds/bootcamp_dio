32f632d1fca268a94e06f06d4d986f689d6e063e
rafael-ds <radisilvads@gmail.com>
2022-06-12 07:31:53 -0300

    Craindo uma tabela

--- Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/1-introducao_com_angular8/course-manager/src/app/couser/couser-list.components.ts
+++ Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/1-introducao_com_angular8/course-manager/src/app/couser/couser-list.components.ts
@@ -1,10 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
     selector: 'app-course-list',
     // template: '<h2> Course List </h2>' 
     templateUrl: './couser-list.components.html' 
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+    courses: Course[] = []
 
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageURL: '',
+                price: 99.99,
+                code: 'xps-9-8796',
+                duration: 26,
+                rating: 5.4,
+                releaseDate: '2 Novembro 2019'
+
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageURL: '',
+                price: 45.99,
+                code: 'lks-9-3596',
+                duration: 88,
+                rating: 4,
+                releaseDate: '4 Dezembro 2019'
+
+            }
+        ]
+    }
 }
