32ad26ee5e22e96ab00c648da39c827ac2c34ac1
rafael-ds <radisilvads@gmail.com>
2022-06-13 07:21:31 -0300

    Tratando eventos databinding

--- Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/1-introducao_com_angular8/course-manager/src/app/couser/couser-list.components.ts
+++ Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/1-introducao_com_angular8/course-manager/src/app/couser/couser-list.components.ts
@@ -8,11 +8,33 @@ import { CourseService } from "./curse.service";
     templateUrl: './couser-list.components.html' 
 })
 export class CourseListComponent implements OnInit {
-    courses: Course[] = []
+    
+    filteredCourses: Course[] = []
+
+    _courses: Course[] = []
+    
+    _filterBy!: string
+
 
     constructor(private courseService: CourseService) {}
 
     ngOnInit(): void {
-        this.courses = this.courseService.retriveAll()
+        this._courses = this.courseService.retriveAll()
+
+        this.filteredCourses = this._courses
+
+    }
+
+    set filter(value: string){
+        this._filterBy = value
+
+        this.filteredCourses = this._courses.filter(
+                (course: Course) => course.name.toLocaleLowerCase().indexOf(
+                    this._filterBy.toLocaleLowerCase()
+                ) > -1)
+    }
+
+    get filter(){
+        return this._filterBy
     }
 }
