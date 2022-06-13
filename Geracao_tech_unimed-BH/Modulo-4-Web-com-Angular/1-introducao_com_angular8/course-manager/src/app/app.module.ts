970c8a0b2b7895346d3586a2f88b5e73e6846692
rafael-ds <radisilvads@gmail.com>
2022-06-13 08:30:52 -0300

    Protegndo rotas com Guads

--- Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/1-introducao_com_angular8/course-manager/src/app/app.module.ts
+++ Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/1-introducao_com_angular8/course-manager/src/app/app.module.ts
@@ -1,22 +1,38 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './couser/couser-list.components';
 import { StarComponent } from './star/star.component';
 import { ReplecePipe } from './pipe/replece.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Erro404Component } from './erro-404/erro-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplecePipe
+    ReplecePipe,
+    NavBarComponent,
+    Erro404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'course', pathMatch: 'full'
+      },
+      {
+        path: 'course', component: CourseListComponent
+      },
+      {
+        path: '**', component: Erro404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/1-introducao_com_angular8/course-manager/src/app/couser/couser-list.components.ts
+++ Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/1-introducao_com_angular8/course-manager/src/app/couser/couser-list.components.ts
@@ -3,8 +3,6 @@ import { Course } from "./course";
 import { CourseService } from "./curse.service";
 
 @Component({
-    selector: 'app-course-list',
-    // template: '<h2> Course List </h2>' 
     templateUrl: './couser-list.components.html' 
 })
 export class CourseListComponent implements OnInit {
