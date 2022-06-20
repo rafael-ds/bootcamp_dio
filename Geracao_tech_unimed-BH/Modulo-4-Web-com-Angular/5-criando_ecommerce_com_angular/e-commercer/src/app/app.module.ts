2580901a1cd167bbf693177c7d4fdde560beb464
rafael-ds <radisilvads@gmail.com>
2022-06-20 08:06:42 -0300

    Criando components

--- Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/5-criando_ecommerce_com_angular/e-commercer/src/app/app.module.ts
+++ Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/5-criando_ecommerce_com_angular/e-commercer/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+import { FooterComponent } from './component/shared/footer/footer.component';
+import { NavComponent } from './component/shared/nav/nav.component';
+import { BookstoreAppComponent } from './component/bookstore-app/bookstore-app.component';
+import { FiltersComponent } from './component/bookstore-app/filters/filters.component';
+import { ProductListComponent } from './component/bookstore-app/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    BookstoreAppComponent,
+    FiltersComponent,
+    ProductListComponent,
   ],
   imports: [
     BrowserModule
