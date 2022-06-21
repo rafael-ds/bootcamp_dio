1e6c73fc7ca79bb4fffdc13d276bbbae20b5192f
rafael-ds <radisilvads@gmail.com>
2022-06-21 09:00:11 -0300

    Clasula Update

--- Geracao_tech_unimed-BH/Modulo-5-Banco-de-dados-realcional/MySQL_DIO.sql
+++ Geracao_tech_unimed-BH/Modulo-5-Banco-de-dados-realcional/MySQL_DIO.sql
@@ -8,7 +8,11 @@ CREATE TABLE pessoas (
 INSERT INTO pessoa(nome, nascimento) VALUE('Rafael Diego', 'da Silva', '1923-08-19')
 INSERT INTO pessoa(nome, nascimento) VALUE('Raquel', 'Oliveira', '1963-03-09')
 
-
+-- Select
 SELECT * FROM pessoa
 SELECT nome FROM pessoa
 SELECT sobrenome FROM pessoa
+
+
+-- Update
+UPDATE pessoa SET nome='Rafael' WHERE id=1 
