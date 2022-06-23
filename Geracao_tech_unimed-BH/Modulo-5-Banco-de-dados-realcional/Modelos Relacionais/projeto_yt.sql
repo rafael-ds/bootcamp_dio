c18a17b3eaab9c531677206d970f2b72388db307
rafael-ds <radisilvads@gmail.com>
2022-06-23 08:23:37 -0300

    criando e relacionando a tabla seo

--- Geracao_tech_unimed-BH/Modulo-5-Banco-de-dados-realcional/Modelos Relacionais/projeto_yt.sql
+++ Geracao_tech_unimed-BH/Modulo-5-Banco-de-dados-realcional/Modelos Relacionais/projeto_yt.sql
@@ -1,9 +1,9 @@
 CREATE TABLE videos(
-    id INT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_video INT NULL PRIMARY KEY AUTO_INCREMENT,
     autor VARCHAR(30),
     titulo VARCHAR(30),
     likes INT,
-    deslike INT,
+    dislike INT,
 )
 
 INSERT INTO videos(autor, titulo, likes, deslike) VALUES('Kira', 'O livro DN', 30, 5);
@@ -15,7 +15,7 @@ INSERT INTO videos(autor, titulo, likes, deslike) VALUES('Volcan S', 'Cano JJ', 
 --************ Autores *************
 
 CREATE TABLE autor(
-    id INT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_autor INT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(30),
     data_nasciento DATE,
 )
@@ -118,3 +118,111 @@ UPDATE videos SET autor=5 WHERE id_video=7;
 -- +----------+-------+-------------------+-------+----------+
 
 
+-- *********** Join ************
+-- SELECT * FROM videos JOIN autor ON videos.FK_autor = autor.id_autor;
+-- +----------+----------+-------------------+-------+----------+----------+---------+------------+
+-- | id_video | FK_autor | titulo            | likes | dislikes | id_autor | nome    | born       |
+-- +----------+----------+-------------------+-------+----------+----------+---------+------------+
+-- |        1 |        1 | Ricky and Morty   |   530 |        5 |        1 | Kira    | 1992-05-04 |
+-- |        2 |        1 | Pegando o osso    |   310 |       20 |        1 | Kira    | 1992-05-04 |
+-- |        3 |        1 | Porco aranha      |    83 |        1 |        1 | Kira    | 1992-05-04 |
+-- |        4 |        2 | A teoria do acaso |   230 |       36 |        2 | Manu    | 2000-10-03 |
+-- |        5 |        3 | Volcan S          |    54 |        1 |        3 | Romer   | 2010-12-29 |
+-- |        6 |        4 | Mysql             |   148 |        2 |        4 | Sheldon | 0000-00-00 |
+-- |        7 |        5 | Cano JJ           |    54 |        1 |        5 | Radis   | 1997-03-27 |
+-- +----------+----------+-------------------+-------+----------+----------+---------+------------+
+
+
+--************ SEO *************
+
+CREATE TABLE categoria(
+    id_seo INT NULL PRIMARY KEY AUTO_INCREMENT,
+    categoria VARCHAR(30)
+)
+
+-- show tables;
+-- +----------------------------+
+-- | Tables_in_banco_modv_mysql |
+-- +----------------------------+
+-- | autor                      |
+-- | seo                        |
+-- | videos                     |
+-- +----------------------------+
+
+INSERT INTO seo(categoria) VALUES('motos');
+INSERT INTO seo(categoria) VALUES('entreterimentos');
+INSERT INTO seo(categoria) VALUES('pets');
+INSERT INTO seo(categoria) VALUES('programação');
+INSERT INTO seo(categoria) VALUES('outros');
+
+-- select * from seo;
+-- +--------+-----------------+
+-- | id_seo | categoria       |
+-- +--------+-----------------+
+-- |      1 | entreterimentos |
+-- |      2 | motos           |
+-- |      3 | pets            |
+-- |      4 | programação      |
+-- |      5 | outros          |
+-- +--------+-----------------+
+
+--  select * from videos;
+-- +----------+----------+-------------------+--------+-------+----------+
+-- | id_video | FK_autor | titulo            | FK_seo | likes | dislikes |
+-- +----------+----------+-------------------+--------+-------+----------+
+-- |        1 |        1 | Ricky and Morty   |      0 |   530 |        5 |
+-- |        2 |        1 | Pegando o osso    |      0 |   310 |       20 |
+-- |        3 |        1 | Porco aranha      |      0 |    83 |        1 |
+-- |        4 |        2 | A teoria do acaso |      0 |   230 |       36 |
+-- |        5 |        3 | Volcan S          |      0 |    54 |        1 |
+-- |        6 |        4 | Mysql             |      0 |   148 |        2 |
+-- |        7 |        5 | Cano JJ           |      0 |    54 |        1 |
+-- +----------+----------+-------------------+--------+-------+----------+
+
+UPDATE videos SET FK_seo=1 WHERE id_video=1;
+UPDATE videos SET FK_seo=1 WHERE id_video=3;
+UPDATE videos SET FK_seo=1 WHERE id_video=4;
+
+--  select * from videos;
+-- +----------+----------+-------------------+--------+-------+----------+
+-- | id_video | FK_autor | titulo            | FK_seo | likes | dislikes |
+-- +----------+----------+-------------------+--------+-------+----------+
+-- |        1 |        1 | Ricky and Morty   |      1 |   530 |        5 |
+-- |        2 |        1 | Pegando o osso    |      0 |   310 |       20 |
+-- |        3 |        1 | Porco aranha      |      1 |    83 |        1 |
+-- |        4 |        2 | A teoria do acaso |      1 |   230 |       36 |
+-- |        5 |        3 | Volcan S          |      0 |    54 |        1 |
+-- |        6 |        4 | Mysql             |      0 |   148 |        2 |
+-- |        7 |        5 | Cano JJ           |      0 |    54 |        1 |
+-- +----------+----------+-------------------+--------+-------+----------+
+
+UPDATE videos SET FK_seo=2 WHERE id_video=5;
+UPDATE videos SET FK_seo=2 WHERE id_video=7;
+-- select * from videos;
+-- +----------+----------+-------------------+--------+-------+----------+
+-- | id_video | FK_autor | titulo            | FK_seo | likes | dislikes |
+-- +----------+----------+-------------------+--------+-------+----------+
+-- |        1 |        1 | Ricky and Morty   |      1 |   530 |        5 |
+-- |        2 |        1 | Pegando o osso    |      0 |   310 |       20 |
+-- |        3 |        1 | Porco aranha      |      1 |    83 |        1 |
+-- |        4 |        2 | A teoria do acaso |      1 |   230 |       36 |
+-- |        5 |        3 | Volcan S          |      2 |    54 |        1 |
+-- |        6 |        4 | Mysql             |      0 |   148 |        2 |
+-- |        7 |        5 | Cano JJ           |      2 |    54 |        1 |
+-- +----------+----------+-------------------+--------+-------+----------+
+
+UPDATE videos SET FK_seo=3 WHERE id_video=2;
+UPDATE videos SET FK_seo=4 WHERE id_video=6;
+
+-- select * from videos;
+-- +----------+----------+-------------------+--------+-------+----------+
+-- | id_video | FK_autor | titulo            | FK_seo | likes | dislikes |
+-- +----------+----------+-------------------+--------+-------+----------+
+-- |        1 |        1 | Ricky and Morty   |      1 |   530 |        5 |
+-- |        2 |        1 | Pegando o osso    |      3 |   310 |       20 |
+-- |        3 |        1 | Porco aranha      |      1 |    83 |        1 |
+-- |        4 |        2 | A teoria do acaso |      1 |   230 |       36 |
+-- |        5 |        3 | Volcan S          |      2 |    54 |        1 |
+-- |        6 |        4 | Mysql             |      4 |   148 |        2 |
+-- |        7 |        5 | Cano JJ           |      2 |    54 |        1 |
+-- +----------+----------+-------------------+--------+-------+----------+
