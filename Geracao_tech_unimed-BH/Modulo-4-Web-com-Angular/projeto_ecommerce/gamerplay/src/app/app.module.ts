8dfd402f7f55b845cc8a1c4deddb230b220ef3a0
rafael-ds <radisilvads@gmail.com>
2022-06-24 18:37:15 -0300

    Criando os components do header

--- Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/projeto_ecommerce/gamerplay/src/app/app.module.ts
+++ Geracao_tech_unimed-BH/Modulo-4-Web-com-Angular/projeto_ecommerce/gamerplay/src/app/app.module.ts
@@ -3,18 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './componet/shared/header/header.component';
-import { FooterComponent } from './componet/shared/footer/footer.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+import { FooterComponent } from './component/shared/footer/footer.component';
+import { LogoComponent } from './component/shared/header/logo/logo.component';
+import { MenuComponent } from './component/shared/header/menu/menu.component';
+import { ServicosComponent } from './component/shared/header/servicos/servicos.component';
+import { BuscaComponent } from './component/shared/header/busca/busca.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LogoComponent,
+    MenuComponent,
+    ServicosComponent,
+    BuscaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
