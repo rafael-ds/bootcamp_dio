90fcf934b6e0c305187290ec957a7e40848350c2
rafael-ds <radisilvads@gmail.com>
2022-06-25 08:10:04 -0300

    Criando o primeiro projeto console .NET

---
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/3-primeiros_passos_com_dotNet/DIO/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+int numeroDeVezes = 5;
+
+for(int i = 0; i < numeroDeVezes; i++){
+    Console.WriteLine($"Bem vindo ao curso de .NET {i + 1}");
+};
