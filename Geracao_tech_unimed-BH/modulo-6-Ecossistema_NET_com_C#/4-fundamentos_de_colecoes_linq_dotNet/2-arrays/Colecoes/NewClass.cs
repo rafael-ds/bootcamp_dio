61d8cd85e7c59a11938731bee9f8a176da4784aa
rafael-ds <radisilvads@gmail.com>
2022-06-28 06:58:23 -0300

    Preparando o projeto

---
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/4-fundamentos_de_colecoes_linq_dotNet/2-arrays/Colecoes/NewClass.cs
@@ -0,0 +1,12 @@
+// using System;
+
+
+// namespace Colecoes
+// {
+//     public class Program
+//     {
+//         static void Main(string[] args){
+//             Console.WriteLine("Hello, World!");
+//         }
+//     }
+// }
