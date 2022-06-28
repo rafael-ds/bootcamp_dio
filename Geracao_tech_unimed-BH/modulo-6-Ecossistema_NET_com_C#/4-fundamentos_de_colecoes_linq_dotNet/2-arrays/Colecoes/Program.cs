1e15396030800c05128b45db9a8c622bdb53a726
rafael-ds <radisilvads@gmail.com>
2022-06-28 09:26:46 -0300

    Arrays multidimensionais

--- Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/4-fundamentos_de_colecoes_linq_dotNet/2-arrays/Colecoes/Program.cs
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/4-fundamentos_de_colecoes_linq_dotNet/2-arrays/Colecoes/Program.cs
@@ -1,9 +1,10 @@
-﻿int[] arrayInteiros = new int[3];
+﻿
+// int[] arrayInteiros = new int[3];
 
-arrayInteiros[0] = 10;
-arrayInteiros[1] = 100;
-arrayInteiros[2] = int.Parse("1000"); // convertendo string para int
-arrayInteiros[3] = 2000;
+// arrayInteiros[0] = 10;
+// arrayInteiros[1] = 100;
+// arrayInteiros[2] = int.Parse("1000"); // convertendo string para int
+// arrayInteiros[3] = 2000;
 
 
 // System.Console.WriteLine();
@@ -21,3 +22,20 @@ arrayInteiros[3] = 2000;
 // }
 // System.Console.WriteLine();
 
+
+// Array contem 4 linhas e 2 colunas
+int[,] matriz = new int[4, 2]{
+    {4, 8},  // linha 0
+    {5, 10}, // linha 1
+    {6, 12}, // linha 2
+    {8, 16}, // linha 3
+};
+
+for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+{
+    for (int colunas = 0; colunas < matriz.GetLength(1); colunas++) 
+    {
+        System.Console.WriteLine(matriz[linhas, colunas]);
+    }
+    
+}
