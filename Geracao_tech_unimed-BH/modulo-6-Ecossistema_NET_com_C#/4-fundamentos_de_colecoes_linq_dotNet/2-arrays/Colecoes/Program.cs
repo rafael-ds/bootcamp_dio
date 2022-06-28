b62dfbe85859407aa82f4256d8f553647693a693
rafael-ds <radisilvads@gmail.com>
2022-06-28 07:24:58 -0300

    pecorrendo arrays com for e foreach

--- Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/4-fundamentos_de_colecoes_linq_dotNet/2-arrays/Colecoes/Program.cs
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/4-fundamentos_de_colecoes_linq_dotNet/2-arrays/Colecoes/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 10;
+arrayInteiros[1] = 100;
+arrayInteiros[2] = 1000;
+
+System.Console.WriteLine();
+System.Console.WriteLine("Pecorrendo o array com for");
+for (int i = 0; i < arrayInteiros.Length; i++)
+{
+    System.Console.WriteLine(arrayInteiros[i]);
+}
+
+System.Console.WriteLine();
+System.Console.WriteLine("Pecorrendo o array com foreach");
+foreach (int item in arrayInteiros)
+{
+    System.Console.WriteLine(item);
+}
+System.Console.WriteLine();
+
