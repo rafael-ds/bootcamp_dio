d17c676c08c658304c14dfae9fec6152717d4651
rafael-ds <radisilvads@gmail.com>
2022-06-28 09:02:27 -0300

    Acesando arrays de forma segura

--- Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/4-fundamentos_de_colecoes_linq_dotNet/2-arrays/Colecoes/Program.cs
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/4-fundamentos_de_colecoes_linq_dotNet/2-arrays/Colecoes/Program.cs
@@ -2,20 +2,22 @@
 
 arrayInteiros[0] = 10;
 arrayInteiros[1] = 100;
-arrayInteiros[2] = 1000;
+arrayInteiros[2] = int.Parse("1000"); // convertendo string para int
+arrayInteiros[3] = 2000;
 
-System.Console.WriteLine();
-System.Console.WriteLine("Pecorrendo o array com for");
-for (int i = 0; i < arrayInteiros.Length; i++)
-{
-    System.Console.WriteLine(arrayInteiros[i]);
-}
 
-System.Console.WriteLine();
-System.Console.WriteLine("Pecorrendo o array com foreach");
-foreach (int item in arrayInteiros)
-{
-    System.Console.WriteLine(item);
-}
-System.Console.WriteLine();
+// System.Console.WriteLine();
+// System.Console.WriteLine("Pecorrendo o array com for");
+// for (int i = 0; i < arrayInteiros.Length; i++)
+// {
+//     System.Console.WriteLine(arrayInteiros[i]);
+// }
+
+// System.Console.WriteLine();
+// System.Console.WriteLine("Pecorrendo o array com foreach");
+// foreach (int item in arrayInteiros)
+// {
+//     System.Console.WriteLine(item);
+// }
+// System.Console.WriteLine();
 
