7844928882c7245dd9fdda7bb8bfc67658fca886
rafael-ds <radisilvads@gmail.com>
2022-06-29 06:14:02 -0300

    manipulando arrays

--- Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/4-fundamentos_de_colecoes_linq_dotNet/3-manipulando_arrays/manipulandoArrays/Program.cs
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/4-fundamentos_de_colecoes_linq_dotNet/3-manipulando_arrays/manipulandoArrays/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] matriz = new int[4, 2]{
+    {4, 8},  // linha 0
+    {5, 10}, // linha 1
+    {6, 12}, // linha 2
+    {8, 16}, // linha 3
+};
+
+for (int linhas = 0; linhas < matriz.GetLength(0); linhas++)
+{
+    for (int colunas = 0; colunas < matriz.GetLength(1); colunas++) 
+    {
+        System.Console.WriteLine(matriz[linhas, colunas]);
+    }
+    
+}
