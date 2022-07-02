0e7a61fb7f0b9f0875985eca42d10d52ab1ed8f8
rafael-ds <radisilvads@gmail.com>
2022-07-02 09:21:11 -0300

    DirectoryInfo

--- Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/6-trabalhando_com_arquivos_e_strems/2-diretorios/projeto/Program.cs
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/6-trabalhando_com_arquivos_e_strems/2-diretorios/projeto/Program.cs
@@ -1,18 +1,62 @@
-﻿var path = Path.Combine(Environment.CurrentDirectory, "globo");
-var dirGlobo = Directory.CreateDirectory(path);
+﻿CriarDiretorioGlobo();
+// CriarArquivo();
 
-var dirAmNorte =  dirGlobo.CreateSubdirectory("America do Norte");
-var dirAmCentral = dirGlobo.CreateSubdirectory("America Central");
-var dirAmSul = dirGlobo.CreateSubdirectory("America do Sul");
+var origem = Path.Combine(Environment.CurrentDirectory, "Brasil.txt");
+var destino = Path.Combine(Environment.CurrentDirectory, "globo", "America do Sul", "Brasil", "Brasil.txt");
+MoverArquivo(origem, destino);
 
-dirAmNorte.CreateSubdirectory("USA");
-dirAmNorte.CreateSubdirectory("Mexico");
-dirAmNorte.CreateSubdirectory("Canadá");
+static void MoverArquivo(string pathOrigem, string pathDestino)
+{
+    if (!File.Exists(pathOrigem))
+    {
+        Console.WriteLine("O aquivo não existe.");
+        return;
+    }
 
-dirAmCentral.CreateSubdirectory("Costa Rica");
-dirAmCentral.CreateSubdirectory("Panama");
+    if (File.Exists(pathDestino))
+    {
+        Console.WriteLine("O aquivo já existe.");
+        return;
+    }
+    File.Move(pathOrigem, pathDestino);
+}
 
-dirAmSul.CreateSubdirectory("Brasil");
-dirAmSul.CreateSubdirectory("Argentina");
-dirAmSul.CreateSubdirectory("Paraguai");
+static void CriarArquivo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "Brasil.txt");
+    if (!File.Exists(path))
+    {
+        using var sw = File.CreateText(path);
+        sw.WriteLine("População: 213M");
+        sw.WriteLine("IDM: 0,901");
+        sw.WriteLine("Dados atualizado em: 0/0/0");
+
+    }
+}
+
+static void CriarDiretorioGlobo()
+{
+    var path = Path.Combine(Environment.CurrentDirectory, "globo");
+    if (!Directory.Exists(path))
+    {
+
+        var dirGlobo = Directory.CreateDirectory(path);
+
+        var dirAmNorte = dirGlobo.CreateSubdirectory("America do Norte");
+        var dirAmCentral = dirGlobo.CreateSubdirectory("America Central");
+        var dirAmSul = dirGlobo.CreateSubdirectory("America do Sul");
+
+        dirAmNorte.CreateSubdirectory("USA");
+        dirAmNorte.CreateSubdirectory("Mexico");
+        dirAmNorte.CreateSubdirectory("Canadá");
+
+        dirAmCentral.CreateSubdirectory("Costa Rica");
+        dirAmCentral.CreateSubdirectory("Panama");
+
+        dirAmSul.CreateSubdirectory("Brasil");
+        dirAmSul.CreateSubdirectory("Argentina");
+        dirAmSul.CreateSubdirectory("Paraguai");
+    }
+}
+
 
