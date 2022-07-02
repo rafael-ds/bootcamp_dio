3cc5542b2bedd8221ce31c26c1fb93fa50660dd0
rafael-ds <radisilvads@gmail.com>
2022-07-02 07:54:48 -0300

    tratamentos de erros

--- Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/6-trabalhando_com_arquivos_e_strems/arquivos_e_streams/Program.cs
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/6-trabalhando_com_arquivos_e_strems/arquivos_e_streams/Program.cs
@@ -1,11 +1,45 @@
-﻿var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
+﻿using static System.Console;
 
-// File.Create(path);
+WriteLine("Digite o nome do arquivo: ");
+var nome = ReadLine();
+nome = tratarCaracter(nome);
 
-using var sw = File.CreateText(path);
-sw.WriteLine("Esta é a linha 1");
-sw.WriteLine("Esta é a linha 2");
-sw.WriteLine("Esta é a linha 3");
-sw.WriteLine("Esta é a linha 4");
-sw.WriteLine("Esta é a linha 5");
-// sw.Flush();
+var path = Path.Combine(Environment.CurrentDirectory, $"{nome}.txt");
+CriarArquivo(path);
+
+WriteLine("Digite enter para finalizar: ");
+ReadLine();
+
+
+static string tratarCaracter(string nome)
+{
+    foreach (var caracter in Path.GetInvalidFileNameChars())
+    {
+        nome = nome.Replace(caracter, '-');
+    }
+    return nome;
+
+}
+
+
+static void CriarArquivo(string path)
+{
+    try
+    {
+        // File.Create(path);
+        using var sw = File.CreateText(path);
+        sw.WriteLine("Esta é a linha 1");
+        sw.WriteLine("Esta é a linha 2");
+        sw.WriteLine("Esta é a linha 3");
+        sw.WriteLine("Esta é a linha 4");
+        sw.WriteLine("Esta é a linha 5");
+        // sw.Flush();
+
+    }
+    catch
+    {
+        WriteLine("O nome do arquivo é inválido.");
+
+    }
+
+}
