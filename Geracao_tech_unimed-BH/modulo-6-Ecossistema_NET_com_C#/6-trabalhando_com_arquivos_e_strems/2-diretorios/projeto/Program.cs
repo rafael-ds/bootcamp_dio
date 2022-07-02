bd17c0cfacd9d0742007ca1ed2e34ee1fbe77726
rafael-ds <radisilvads@gmail.com>
2022-07-02 08:15:11 -0300

    Criando diretorios

---
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/6-trabalhando_com_arquivos_e_strems/2-diretorios/projeto/Program.cs
@@ -0,0 +1,18 @@
+﻿var path = Path.Combine(Environment.CurrentDirectory, "globo");
+var dirGlobo = Directory.CreateDirectory(path);
+
+var dirAmNorte =  dirGlobo.CreateSubdirectory("America do Norte");
+var dirAmCentral = dirGlobo.CreateSubdirectory("America Central");
+var dirAmSul = dirGlobo.CreateSubdirectory("America do Sul");
+
+dirAmNorte.CreateSubdirectory("USA");
+dirAmNorte.CreateSubdirectory("Mexico");
+dirAmNorte.CreateSubdirectory("Canadá");
+
+dirAmCentral.CreateSubdirectory("Costa Rica");
+dirAmCentral.CreateSubdirectory("Panama");
+
+dirAmSul.CreateSubdirectory("Brasil");
+dirAmSul.CreateSubdirectory("Argentina");
+dirAmSul.CreateSubdirectory("Paraguai");
+
