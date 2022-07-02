14ce67f089cbf64ecf6a3f4b52b74b3bbf3f9fa3
rafael-ds <radisilvads@gmail.com>
2022-07-02 07:29:00 -0300

    criando arquivos

---
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/6-trabalhando_com_arquivos_e_strems/arquivos_e_streams/Program.cs
@@ -0,0 +1,11 @@
+﻿var path = Path.Combine(Environment.CurrentDirectory, "teste.txt");
+
+// File.Create(path);
+
+using var sw = File.CreateText(path);
+sw.WriteLine("Esta é a linha 1");
+sw.WriteLine("Esta é a linha 2");
+sw.WriteLine("Esta é a linha 3");
+sw.WriteLine("Esta é a linha 4");
+sw.WriteLine("Esta é a linha 5");
+// sw.Flush();
