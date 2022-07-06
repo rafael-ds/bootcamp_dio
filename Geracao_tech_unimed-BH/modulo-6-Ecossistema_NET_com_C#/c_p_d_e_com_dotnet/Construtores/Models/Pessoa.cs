a2fc5fab68a56071224414693e142217ed4d4eca
rafael-ds <radisilvads@gmail.com>
2022-07-06 06:59:14 -0300

    Criando construtores

---
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/c_p_d_e_com_dotnet/Construtores/Models/Pessoa.cs
@@ -0,0 +1,25 @@
+namespace Construtores.Models
+{
+    public class Pessoa
+    {
+        
+        private string nome;
+        private string sobrenome;
+
+        public Pessoa()
+        {
+            nome = string.Empty;
+            sobrenome = string.Empty;
+        }
+
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+        }
+
+        public void Apresentar(){
+            System.Console.WriteLine($"Olá meu nome é {nome} {sobrenome}");
+        }
+    }
+}
--- Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/c_p_d_e_com_dotnet/Construtores/Program.cs
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/c_p_d_e_com_dotnet/Construtores/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Construtores.Models;
+
+Pessoa p1 = new Pessoa();
+p1.Apresentar();
