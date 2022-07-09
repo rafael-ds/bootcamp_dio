7472cb612264e04b095fe4675603e4a4327d76cd
rafael-ds <radisilvads@gmail.com>
2022-07-09 16:23:40 -0300

    Refatorando o menu do soldado

--- Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/Projeto_dotNet/abstraindo_rpg_com_csharp/SaintSeiya/Models/Characters/Soldier/MenuSoldier.cs
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/Projeto_dotNet/abstraindo_rpg_com_csharp/SaintSeiya/Models/Characters/Soldier/MenuSoldier.cs
@@ -5,37 +5,12 @@ namespace SaintSeiya.Models.Characters.Soldiers
         public void Option()
         {
             Soldiers soldier = new Soldiers();
+            AttackDefend attackAndDefend = new AttackDefend();
+            
             soldier.Soldier();
             System.Console.WriteLine($"\n{soldier}\n");
 
-            while (true)
-            {
-                System.Console.WriteLine($"================ Soldado ================");
-
-                System.Console.WriteLine(" Digite (1) para Atacar. ");
-                System.Console.WriteLine(" Digite (2) para Defender. ");
-                System.Console.WriteLine(" Digite (0) para escoler outro personagem. ");
-
-                System.Console.WriteLine($"===============================================");
-
-                string? opcao = System.Console.ReadLine();
-
-                switch (opcao)
-                {
-                    case "1":
-                        System.Console.WriteLine(soldier.LaunchAttack());
-                        break;
-                    case "2":
-                        System.Console.WriteLine(soldier.Defend());
-                        break;
-                }
-
-                if (opcao == "0")
-                {
-                    System.Console.WriteLine("\nRetornando ao menu...\n");
-                    break;
-                }
-            }
+            attackAndDefend.attackDefend(soldier);
         }
     }
 }
