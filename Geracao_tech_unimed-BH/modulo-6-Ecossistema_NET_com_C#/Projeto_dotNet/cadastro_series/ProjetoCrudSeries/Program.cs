fabacdbef2f74ec002860ce773bf9b4950ba6e91
rafael-ds <radisilvads@gmail.com>
2022-07-16 07:04:54 -0300

    Estilizando o menu

--- Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/Projeto_dotNet/cadastro_series/ProjetoCrudSeries/Program.cs
+++ Geracao_tech_unimed-BH/modulo-6-Ecossistema_NET_com_C#/Projeto_dotNet/cadastro_series/ProjetoCrudSeries/Program.cs
@@ -48,13 +48,14 @@ namespace ProjetoCrudSeries
 
         private static void ListSeries()
 		{
-			Console.WriteLine("Listar séries");
+			Console.WriteLine(" ===================== Listar séries ======================== ");
+			Console.WriteLine();
 
 			var list = repository.List();
 
 			if (list.Count == 0)
 			{
-				Console.WriteLine("Nenhuma série cadastrada.");
+				Console.WriteLine(" Ops! Nenhuma série cadastrada.");
 				return;
 			}
 
@@ -62,20 +63,21 @@ namespace ProjetoCrudSeries
 			{   
                 var exclude = serie.ReturnExclude();  
 
-                Console.WriteLine("#ID {0}: - {1} {2}", serie.ReturnId(), serie.ReturnTitle(), (exclude ? "*Excluido*" : ""));         
+                Console.WriteLine(" #ID {0}: - {1} {2}", serie.ReturnId(), serie.ReturnTitle(), (exclude ? "*Excluido*" : ""));         
 			}
 		}
 
         private static void InsertSerie(){
-            Console.WriteLine(" Inserir nova série");
+            Console.WriteLine(" ===================== Inserir nova séries ======================== ");
 
+            Console.WriteLine(" ** Categotias ** ");
             foreach (int item in Enum.GetValues(typeof(Genre)))
             {
                 Console.WriteLine(" {0} - {1}", item, Enum.GetName(typeof(Genre), item));
-                // Console.WriteLine();
             }
+            Console.WriteLine();
 
-            Console.WriteLine(" Digite o gênero apartir da opções informada acima: ");
+            Console.WriteLine(" Digite a categoria apartir da opções informada acima: ");
             int enterGenre = int.Parse(Console.ReadLine());
 
             Console.WriteLine(" Digite o titulo da serie: ");
@@ -99,7 +101,9 @@ namespace ProjetoCrudSeries
         }
 
         private static void UpedateSerie(){
-            Console.WriteLine("Informe o Id da serie: ");
+            Console.WriteLine(" ===================== Atualizar uma séries ======================== ");
+
+            Console.WriteLine(" **Informe o Id da serie: **");
             int indiceSerie = int.Parse(Console.ReadLine());
 
 
@@ -132,7 +136,9 @@ namespace ProjetoCrudSeries
         }
 
         private static void DeleteSerie(){
-            Console.WriteLine("Digite o Id da série: ");
+            Console.WriteLine(" ===================== Deletar uma séries ======================== ");
+
+            Console.WriteLine(" **Digite o Id da série: ** ");
             int indiceSerie = int.Parse(Console.ReadLine());
 
             repository.Delete(indiceSerie);
@@ -140,7 +146,9 @@ namespace ProjetoCrudSeries
 
 
         private static void ViewSerie(){
-            Console.WriteLine("Digite o Id da série");
+            Console.WriteLine(" ===================== Informações da série ======================== ");
+
+            Console.WriteLine(" **Digite o Id da série : ** ");
             int indiceSerie = int.Parse(Console.ReadLine());
 
             var serie = repository.ReturnId(indiceSerie);
@@ -151,8 +159,9 @@ namespace ProjetoCrudSeries
          private static string Menu()
 		{
 			Console.WriteLine();
-			Console.WriteLine(" Super Serie Crud ");
+			Console.WriteLine(" ===================== Super Serie Crud ======================== ");
 			Console.WriteLine(" Digite a opção desejada: ");
+			Console.WriteLine();
 
 			Console.WriteLine(" 1- Listar séries "); 
 			Console.WriteLine(" 2- Inserir nova série ");
@@ -161,6 +170,7 @@ namespace ProjetoCrudSeries
 			Console.WriteLine(" 5- Visualizar série ");
 			Console.WriteLine(" C- Limpar Tela ");
 			Console.WriteLine(" X- Sair");
+			Console.WriteLine(" ================================================================= ");
 			Console.WriteLine();
 
 			string userOption = Console.ReadLine().ToUpper();
