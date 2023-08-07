16cc5e9310e128a50a83cb3aa97751ac7937431f
Rafael Silva <radisilvads@gmail.com>
2023-08-07 19:14:37 -0300

    Desenvolvendo o menu so sitema

--- Potência Tech powered by iFood  Ciência de Dados/desafio_criando_um_sistema_bancario/sistema_banco.py
+++ Potência Tech powered by iFood  Ciência de Dados/desafio_criando_um_sistema_bancario/sistema_banco.py
@@ -1,1 +1,49 @@
-print("Sistema bancário")
+
+print('\n')
+print('='*50)
+print('Desafio de projeto - - Sistema Bancário')
+print('='*50)
+print('\n')
+
+
+def sacar_valor():
+    print('sacar')
+
+def depositar_valor():
+    print('depositar')
+
+def extrato_bancario():
+    print('extrato')
+
+
+while True:
+    print('Informe a operação desejada:\n')
+
+    print(' [1] - Sacar')
+    print(' [2] - Depositar')
+    print(' [3] - Extrato')
+    print(' [4] - sair')
+
+    sacar = '1'
+    depositar = '2'
+    extrato = '3'
+    sair = '4'
+
+    entrada = input()
+
+    if entrada == sacar:
+        sacar_valor()
+
+    elif entrada == depositar:
+        depositar_valor()
+
+    elif entrada == extrato:
+       extrato_bancario()
+
+    elif entrada == sair:
+        break
+    else:
+        print('Favor digitar um opção valida\n')
+
+
+
