66235057b5f3dcdf5638ba98d16b49953a15200f
Rafael Silva <radisilvads@gmail.com>
2023-08-13 17:09:03 -0300

    Finalização do projeto

--- Potência Tech powered by iFood  Ciência de Dados/desafio_criando_um_sistema_bancario/sistema_banco.py
+++ Potência Tech powered by iFood  Ciência de Dados/desafio_criando_um_sistema_bancario/sistema_banco.py
@@ -1,9 +1,14 @@
+# Projeto proposto pelo bootcamp Dio.me Potência Tech poered by iFood | Ciência de Dados
+# Objetivo é realizar um sistema bancario que simule as principais realizações como:
+# Depositar - Sacar e Visualizar extratos
+# Na resolução do desafio foi usado o funções no desenvolvimento do projeto.
+
 conta = 0
 
 extrato_depositos = []
 extrato_saques = []
 
-# Função banner que tem como objetivo somente informar em que area o úsuario se encontra
+
 def banner(texto):
     print('\n')
     print('='*50)
@@ -16,21 +21,43 @@ banner(' Desafio de projeto - - Sistema Bancário ')
 
 
 def sacar_valor():
-    print('sacar')
-
-
-
-# Função que tem como objetivo depositar um valor na lista Conta[]
-def depositar_valor():
     global conta
 
-    banner(' Depósitar ')
+    print('sacar')
+
+    print(' Informe o valor que deseja sacar: ')
+    valor_saque = float(input())
+
+    confirmar = input(f' Valor a ser depositado é de R${valor_saque} Pressione:\n [S] para confirmar o deposito\n [N] para cancelar o deposito \n')
+    
+    if confirmar == 's':
+
+        if conta > valor_saque:
+            saque = conta - valor_saque
+            conta = saque
+
+            extrato_saques.append(valor_saque)
+
+        else:
+            print(' Saldo insuficiente! ')
+            print(f' Saldo atual R$ {conta}')
+
+    elif confirmar == 'n':
+        print(' Saque cancelado...\n')
+    else:
+        print(' Porfavor informe um opção valida\n')
+
+
+def depositar_valor():
+    global conta # Foi necessario para que ouvesse conexão com o a variavel conta do escopo global
+
+    banner(' Depositar ')
 
     print(" Informe o valor a ser depositado:\n ")
 
     valor_deposito = float(input())
 
-    confirmar = input(f' Valor para depósito é de R${valor_deposito} Pressione:\n [S] para comfirmar o deposito\n [N] para cancelar o deposito \n')
+    confirmar = input(f' Valor para depósito é de R${valor_deposito} Pressione:\n [S] para confirmar o deposito\n [N] para cancelar o deposito \n')
     
     if confirmar == 's':
 
@@ -38,19 +65,18 @@ def depositar_valor():
         extrato_depositos.append(valor_deposito)
 
         print(f'\n R${valor_deposito} depósitado com sucesso!')
-        # Reesibir extrato apos o processo
         
 
     elif confirmar == 'n':
-        print('Deposito cancelado\n')
+        print(' Deposito cancelado\n')
     else:
-        print('Porfavor informe um opção valida\n')
+        print(' Porfavor informe um opção válida\n')
 
 
 def extrato_bancario():
     banner('Extrato')
 
-    print('-'*25, 'Depósitos', '-'*25)
+    print('-'*25, 'Depositos', '-'*25)
 
     for i in extrato_depositos:
         print(f' R$ {i}')
@@ -63,10 +89,8 @@ def extrato_bancario():
         print(f' R$ {i}')
 
     print('-'*60)
-
-
-    saldo_em_conta = sum(conta)    
-    return f' Saldo em conta R$ {saldo_em_conta}'
+   
+    return f' Saldo em conta R$ {conta}'
 
 
 while True:
@@ -94,13 +118,12 @@ while True:
         depositar_valor()
 
     elif entrada == extrato:
-        # extrato_bancario()
         print(extrato_bancario())
 
     elif entrada == sair:
         break
     else:
-        print('Favor digitar um opção valida\n')
+        print('Favor digitar um opção válida\n')
 
 
 
